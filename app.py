4b7b98288da05a784ec8b7d5cd932423e87c4c20
Sai Goutham Chedhella <saigouthamch27@gmail.com>
2024-07-09 13:12:58 -0500

    application is connected to mongodb atlas

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from mongoengine import connect
 import os
-
-bcrypt = Bcrypt()
-jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
-    # Dynamically load configuration based on environment variable
+
+    # Load configuration
     config_name = os.getenv('FLASK_CONFIG', 'DevelopmentConfig')
     app.config.from_object(f'config.{config_name}')
-    
+
     CORS(app)
-    
-    bcrypt.init_app(app)
-    jwt.init_app(app)
-    connect(
-        db=app.config['MONGODB_SETTINGS']['db'],
-        host=app.config['MONGODB_SETTINGS']['host'],
-        port=app.config['MONGODB_SETTINGS']['port']
-    )
-    
-    with app.app_context():
-        # Import routes
-        from routes import home
+    bcrypt = Bcrypt(app)
+    jwt = JWTManager(app)
+
+    # Import and register routes
+    from routes.routes import register_routes
+    register_routes(app)
+
+    @app.errorhandler(Exception)
+    def handle_error(error):
+        message = str(error)
+        code = 500  # Internal Server Error
+
+        # Handle specific exceptions and set appropriate error codes
+        if isinstance(error, KeyError):
+            message = "Missing required data"
+            code = 400  # Bad Request
+
+        return jsonify({"error": message}), code
 
     return app
+
+# Run the app if called directly
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- models.py
+++
@@ -1,6 +0,0 @@
-# models.py
-from mongoengine import Document, StringField
-
-class User(Document):
-    username = StringField(required=True, unique=True)
-    password = StringField(required=True)
