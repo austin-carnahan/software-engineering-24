335cf6cb0bc915226c960c8a0ae0104eda0f71af
Raghunath Kunigiri <54011917+Raghunath9493@users.noreply.github.com>
2024-08-05 19:50:34 -0500

    Reset_link

--- email_utils.py
+++ email_utils.py
@@ -1,22 +1,29 @@
 # email_utils.py
 
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 def send_email(subject, content, to_email):
     HOST = "smtp-mail.outlook.com"
     PORT = 587
     FROM_EMAIL = "Kunigiriraghunath9493@gmail.com"  # Replace with your email address
     PASSWORD = "Kunimicro@1998"
 
-    message = """Subject: Reset Password
-    Hello 
-    This is your password
-    
-    Thanks,
-    SE_G5_Team"""
+    # Create the email content
+    message = MIMEMultipart()
+    message['From'] = FROM_EMAIL
+    message['To'] = to_email
+    message['Subject'] = subject
 
+    # Attach the email body
+    message.attach(MIMEText(content, 'plain'))
+
+
+ # Convert the message to a string and send it
     with smtplib.SMTP(HOST, PORT) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.login(FROM_EMAIL, PASSWORD)
-        smtp.sendmail(FROM_EMAIL, to_email, message)
+        smtp.sendmail(FROM_EMAIL, to_email, message.as_string())
 
